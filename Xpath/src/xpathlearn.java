152900787727cd88f583ee66fed20fef9b4f46ae
Ranjith <Ranjith@Jesus-PC>
2020-06-08 14:08:03 +0530

    Initial Commit

---
+++ Xpath/src/xpathlearn.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class xpathlearn {
+	//To write and check Xpath in Chrome
+	//Press F12 to open Chrome Developer Tool
+	//In "Elements" panel, press Ctrl+F
+	@Test
+	public void OrangeHRM() {
+		WebDriver driver = new FirefoxDriver();//ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/index.php/auth/login");
+
+		driver.findElement(By.xpath("//*[@name='txtUsername']")).sendKeys("Admin");
+		driver.findElement(By.xpath("//*[@name='txtPassword']")).sendKeys("admin123");
+		driver.quit();
+	}
+}
